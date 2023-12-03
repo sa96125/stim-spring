a674d226467fb42ea3d6453dc87c80b828dc914b
sa96125 <sa96125@gmail.com>
2023-12-03 12:51:36 +0900

    [feat] 레디스 설정 및 메서드 구현

---
+++ src/main/java/com/sa96125/stim/common/redis/RedisClient.java
@@ -0,0 +1,36 @@
+package com.sa96125.stim.common.redis;
+
+import com.sa96125.stim.common.api.exception.custom.ResourceNotFoundException;
+import com.sa96125.stim.domain.auth.controller.port.SessionAdapter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RedisClient implements SessionAdapter {
+    
+    private final StringRedisTemplate stringRedisTemplate;
+    
+    private static final String VERIFICATION_CODE_KEY_PREFIX = "verificationCode-";
+    
+    @Override
+    public String saveFor3Minutes(String verificationCode) {
+        String accessKey = UUID.randomUUID().toString();
+        stringRedisTemplate.opsForValue().set( VERIFICATION_CODE_KEY_PREFIX + accessKey , verificationCode, 3, TimeUnit.MINUTES);
+        return accessKey;
+    }
+    
+    @Override
+    public void validateMobile(String key, String verificationCode) {
+        if (!Objects.equals(stringRedisTemplate.opsForValue().get(VERIFICATION_CODE_KEY_PREFIX + key), verificationCode)) {
+            throw new ResourceNotFoundException("Mobile validation failed: Verification code does not match");
+        }
+    }
+}
---
+++ src/main/java/com/sa96125/stim/common/redis/RedisConfig.java
@@ -0,0 +1,53 @@
+package com.sa96125.stim.common.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    
+    @Value("${spring.data.redis.host}")
+    private String host;
+    
+    @Value("${spring.data.redis.password}")
+    private String password;
+    
+    @Value("${spring.data.redis.port}")
+    private int port;
+    
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+    
+    @Bean
+    public StringRedisTemplate stringRedisTemplate() {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setKeySerializer(new StringRedisSerializer());
+        stringRedisTemplate.setValueSerializer(new StringRedisSerializer());
+        stringRedisTemplate.setConnectionFactory(redisConnectionFactory());
+        return stringRedisTemplate;
+    }
+    
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setPort(port);
+        configuration.setPassword(password);
+        configuration.setHostName(host);
+        return new LettuceConnectionFactory(configuration);
+    }
+    
+}
