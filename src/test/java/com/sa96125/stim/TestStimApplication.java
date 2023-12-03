17a9f3265a974e3cd972c28120db9354a710f147
sa96125 <sa96125@gmail.com>
2023-12-03 12:53:56 +0900

    [fix] 불필요 파일 삭제 및 메타 정보 업데이트

--- src/test/java/com/sa96125/stim/TestStimApplication.java
+++ src/test/java/com/sa96125/stim/TestStimApplication.java
@@ -1,14 +1,13 @@
-package com.sa96125.stimspring;
+package com.sa96125.stim;
 
-import com.sa96125.stim.Stim;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 
 @TestConfiguration(proxyBeanMethods = false)
-public class TestStimSpringApplication {
+public class TestStimApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(Stim::main).with(TestStimSpringApplication.class).run(args);
+		SpringApplication.from(Stim::main).with(TestStimApplication.class).run(args);
 	}
 
 }
