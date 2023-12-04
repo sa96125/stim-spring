0ef9dd9b0c4aa9c9e21ee306a15ea24429f91d7e
sa96125 <sa96125@gmail.com>
2023-12-04 17:59:49 +0900

    [feat] 씨큐리티 인증 확인 인터페이스 추가

---
+++ src/main/java/com/sa96125/stim/common/security/SecurityContextProvider.java
@@ -0,0 +1,23 @@
+package com.sa96125.stim.common.security;
+
+import com.sa96125.stim.common.api.exception.custom.AuthenticationFailedException;
+import com.sa96125.stim.domain.feed.service.port.SecurityContextAdapter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SecurityContextProvider implements SecurityContextAdapter {
+    public String authenticateUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new AuthenticationFailedException("Authentication failed");
+        }
+        
+        return authentication.getName();
+    }
+}
---
+++ src/main/java/com/sa96125/stim/domain/feed/service/port/SecurityContextAdapter.java
@@ -0,0 +1,5 @@
+package com.sa96125.stim.domain.feed.service.port;
+
+public interface SecurityContextAdapter {
+    String authenticateUser();
+}
